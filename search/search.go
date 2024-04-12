7f6f16a74ab730f6bcfc0759439f41253d794be3
rdiazrovira <rdiaz@wawand.co>
2024-04-12 12:06:22 -0500

    Add a few changes to handle multi-page scraping.

--- search/search.go
+++ search/search.go
@@ -6,6 +6,7 @@ import (
 	"net/url"
 	"os"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/cretz/bine/tor"
@@ -35,6 +36,12 @@ func SearchDarkWeb(keyword string) {
 
 	c.SetRequestTimeout(5 * time.Minute)
 
+	c.Limit(&colly.LimitRule{
+		Parallelism: 2,
+	})
+
+	var wg sync.WaitGroup
+
 	c.OnHTML("body", func(e *colly.HTMLElement) {
 		if strings.Contains(strings.ToLower(e.Text), strings.ToLower(keyword)) {
 			// Getting the whole page for now.
@@ -43,17 +50,31 @@ func SearchDarkWeb(keyword string) {
 		} else {
 			fmt.Printf("Keyword '%s' was not found. \n", keyword)
 		}
+
+		wg.Done()
 	})
 
-	// libreddit
-	onionPage := fmt.Sprintf("http://ecue64yqdxdk3ucrmm2g3irhlvey3wkzcokwi6oodxxwezqk3ak3fhyd.onion/r/popular/search?restrict_sr=on&q=%s", url.QueryEscape(keyword))
+	c.OnError(func(_ *colly.Response, err error) {
+		fmt.Println("Something went wrong:", err)
+	})
+
+	// List of URLs to scrape
+	urls := []string{
+		fmt.Sprintf("http://ecue64yqdxdk3ucrmm2g3irhlvey3wkzcokwi6oodxxwezqk3ak3fhyd.onion/r/popular/search?restrict_sr=on&q=%s", url.QueryEscape(keyword)),
+	}
 
 	fmt.Printf("Sniffing '%s' around in the dark web... \n", keyword)
-	err = c.Visit(onionPage)
-	if err != nil {
-		fmt.Println("Error visiting Onion page:", err)
-		return
+	for _, u := range urls {
+		wg.Add(1)
+
+		err := c.Visit(u)
+		if err != nil {
+			fmt.Println("Error visiting Onion page:", err)
+			return
+		}
 	}
+
+	wg.Wait()
 
 	c.Wait()
 }
